5a366cb56c4f67a474a96ae1143ec469a8f5ac0f
Raphael Guntersweiler <raphael@rgunti.ch>
2017-09-02 13:35:56 +0200

    Finished Template Page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+function renderPage(res, page, title, data) {
+  res.render('templates/main', { page: page, title: title, data: data })
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('templates/main', { title: 'Home' });
+  renderPage(res, 'home', 'Home');
 });
 
 module.exports = router;
