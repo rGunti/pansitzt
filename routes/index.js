4cc5133040797c0c51a9f20c6ed3b53b289d9237
Raphael Guntersweiler <raphael@rgunti.ch>
2017-09-02 13:29:23 +0200

    Added more Init Stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('templates/main', { title: 'Home' });
 });
 
 module.exports = router;
