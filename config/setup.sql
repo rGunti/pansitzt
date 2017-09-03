482a11c09aaef722b12a291401e247c47e575384
Raphael Guntersweiler <raphael@rgunti.ch>
2017-09-03 12:52:34 +0200

    Created Data Model for Posts

--- config/setup.sql
+++ config/setup.sql
@@ -9,3 +9,14 @@ CREATE TABLE users
   updated_at TIMESTAMP
 );
 CREATE UNIQUE INDEX users_twitter_id_uindex ON users (twitter_id);
+
+CREATE TABLE posts
+(
+  id VARCHAR(8) PRIMARY KEY,
+  user_id VARCHAR(20) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  source VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  CONSTRAINT post_author_fk FOREIGN KEY (user_id) REFERENCES users (twitter_id)
+);
---
+++ db/models/Post.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017, Raphael "rGunti" Guntersweiler
+ * Licensed under MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+const Sequelize = require('sequelize');
+const db = require('../dbpool');
+const User = require('./User');
+
+const Post = db.define('posts', {
+    postID: {
+        field: 'id',
+        type: Sequelize.DataTypes.STRING,
+        primaryKey: true,
+        validate: {
+            is: ['[A-Za-z0-9]', 'i']
+        }
+    },
+    author: {
+        field: 'user_id',
+        type: Sequelize.DataTypes.STRING
+    },
+    title: {
+        field: 'title',
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    source: {
+        field: 'source',
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        field: 'created_at',
+        type: Sequelize.DataTypes.TIME
+    },
+    updatedAt: {
+        field: 'updated_at',
+        type: Sequelize.DataTypes.TIME
+    }
+});
+
+Post.belongsTo(User, { foreignKey: 'user_id' });
+
+module.exports = Post;
